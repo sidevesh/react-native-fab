8133ad5602937b3357000610d2ee127966df2a13
Alberto Schiabel <jkomyno@users.noreply.github.com>
2017-05-29 18:18:06 +0200

    Update index.js
    Fixed typo. Lollipop/Android 5 is SDK 21, not 23 as I have written before

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,5 +6,5 @@ const {
 } = Platform;
 
 export const IS_ANDROID = OS === 'android';
-export const IS_LT_LOLLIPOP = Version < 23;
+export const IS_LT_LOLLIPOP = Version < 21;
 export const noop = () => {};
